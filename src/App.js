7842046c3dd7c112b22f9bd8d535d0c1f069554e
NGUYEN HOAI TRUNG <nghtrung1001@gmail.com>
2022-03-08 21:58:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Navbar from './components/Navbar';
+
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
@@ -12,16 +12,15 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <>
       <BrowserRouter>
-      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} /> 
           <Route path="/login" element={<Login />} /> 
         </Routes>
       </BrowserRouter>
-    </div>  
+    </>  
   );
 }
 
